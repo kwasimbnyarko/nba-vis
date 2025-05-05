6618a124826fa15bfb191b514b0e95625ab82d62
Kwasi Biritwum-Nyarko <kwasimbnyarko@gmail.com>
2025-05-04 22:17:17 -0400

    getting player data by category and quarter implemented

--- server/controllers/playersController.js
+++ server/controllers/playersController.js
@@ -6,21 +6,26 @@ import path from "path";
 import {readFile} from "fs/promises";
 
 export const getPlayers = async (req, res) => {
-    const filePath = path.join(dataDirectoryPath, "allPlayers.json")
+    const quarter = req.query.quarter
+    const category = req.query.category
+    const playerId = req.query.playerId
+    const teamId = req.query.teamId
+
+    const extraDir = `./${!!quarter ? "q" + quarter + "/" : ""}`
+    const fileName
+        = `players${!!quarter ? "Q" + quarter : ""}${category?.toLowerCase() === "def" ? "Def" : ""}.json`
+
+    const filePath = path.join(dataDirectoryPath, extraDir + fileName)
     const readData = await readFile(filePath, 'utf8')
     const jsonData = JSON.parse(readData)
     const rowSet = getFriendlierJson(jsonData)
 
     var result = rowSet
 
-    if (req.query.playerId) {
-        result = rowSet.find(player => player.PLAYER_ID === parseInt(req.query.playerId))
-        console.log(result)
-    }
-
-    if (req.query.teamId) {
-        result = rowSet.filter(player => player.TEAM_ID === parseInt(req.query.teamId))
-        console.log(result)
+    if (playerId) {
+        result = rowSet.filter(player => player.PLAYER_ID === parseInt(playerId))
+    } else if (teamId) {
+        result = rowSet.filter(player => player.TEAM_ID === parseInt(teamId))
     }
 
     res.json(result);
